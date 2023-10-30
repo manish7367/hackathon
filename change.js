c4864371fdb39f94a6ee9634ba2213d9ca0067bd
MANISH  KUMAR <106996296+manish7367@users.noreply.github.com>
2023-10-30 12:21:38 +0530

    Add files via upload

---
+++ change.js
@@ -0,0 +1,29 @@
+// function redirect() {
+//     var selectbox = document.getElementById("select-id");
+//     var selectedValue = selectbox.options[selectbox.selectedIndex].value;
+//     if (selectedValue != "") {
+//         window.location.href = selectedValue;
+//     }
+// }
+
+// var selectEl = document.createElement("select");
+// selectEl.id = "select-id";
+// var option1 = document.createElement("option");
+// option1.value = "";
+// option1.text = "Select an option";
+// var option2 = document.createElement("option");
+// option2.value = "https://www.google.com";
+// option2.text = "Google";
+// var option3 = document.createElement("option");
+// option3.value = "https://www.bing.com";
+// option3.text = "Bing";
+// selectEl.add(option1);
+// selectEl.add(option2);
+// selectEl.add(option3);
+
+// var buttonEl = document.createElement("button");
+// buttonEl.textContent = "Go!";
+// buttonEl.onclick = redirect;
+
+// document.body.appendChild(selectEl);
+// document.body.appendChild(buttonEl);
